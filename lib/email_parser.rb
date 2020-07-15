d56542996eae77269d2155ed60a8feb8e1734fb4
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-15 09:35:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,12 +4,14 @@ require'pry'
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :parse
+  
    def initialize parse
      @parse=parse 
    end
    def parse 
      @parse.delete(",").split(" ").uniq
    end
+   
+   
    
  end
